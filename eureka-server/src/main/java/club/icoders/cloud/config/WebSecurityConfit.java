802cf39a3aed916d05741e06c94798ec0b19d159
admin-zhang <764018641@qq.com>
2022-05-29 18:12:51 +0800

    Eureka注册中心添加安全认证

---
+++ eureka-server/src/main/java/club/icoders/cloud/config/WebSecurityConfit.java
@@ -0,0 +1,22 @@
+package club.icoders.cloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhangzb
+ * @Description
+ * @create 2022-05-29 18:07
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfit extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
